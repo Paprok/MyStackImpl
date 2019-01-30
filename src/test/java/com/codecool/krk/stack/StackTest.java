a9946cc01dd683ddbd5b3d7eed5e4f0d8023adbb
Paprok <rafal.becla@gmail.com>
2019-01-30 16:33:26 +0100

    created peek tests

--- src/test/java/com/codecool/krk/stack/StackTest.java
+++ src/test/java/com/codecool/krk/stack/StackTest.java
@@ -117,4 +117,61 @@ public class StackTest {
         assertEquals(actual, expected);
     }
 
+    @Test
+    void testPeekOnEmptyStackThrowsException(){
+        int size = 5;
+        Stack<Integer> stack = new Stack<>(size);
+
+        assertThrows(IndexOutOfBoundsException.class,
+                ()->stack.peek()
+        );
+    }
+
+    @Test
+    void testPeekOn1StringStackSized5(){
+        Stack<String> stack = new Stack<>(5);
+        String expected = "something";
+        stack.push(expected);
+
+        String actual = stack.peek();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testPeekOn1BoolStackSized1(){
+        Stack<Boolean> stack = new Stack<>(1);
+        Boolean expected = true;
+        stack.push(expected);
+
+        Boolean actual = stack.peek();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testPeekOnMidFilledStack(){
+        int size = 10;
+        Stack<Integer> stack = new Stack<>(size);
+        int howMany =6;
+        addItemsToStack(howMany, size, stack);
+
+        int expected = howMany-1;
+        int acutal = stack.peek();
+
+        assertEquals(expected, acutal);
+    }
+
+    @Test
+    void testPeekOnFullStack(){
+        int size = 5;
+        Stack<Integer> stack = new Stack<>(size);
+        addItemsToStack(size, size, stack);
+
+        int expected = 4;
+        int actual = stack.peek();
+
+        assertEquals(expected, actual);
+    }
+    
 }
